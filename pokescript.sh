798e75d357aadef4c7cbb36eef5d521a7c03bd18
Carlos May <97546829+CarlosMay7@users.noreply.github.com>
2025-02-16 22:52:35 -0600

    Update pokescript.sh

--- pokescript.sh
+++ pokescript.sh
@@ -7,21 +7,24 @@ fi
 CSV="pokemon_data.csv"
 
 RESPONSE=$(curl -s "https://pokeapi.co/api/v2/pokemon/$1")
-
-ID=$(echo "$RESPONSE" | jq .id)
-NAME=$(echo "$RESPONSE" | jq -r .name)
-WEIGHT=$(echo "$RESPONSE" | jq .weight)
-HEIGHT=$(echo "$RESPONSE" | jq .height)
-ORDER=$(echo "$RESPONSE" | jq .order)
-
-echo "$NAME (No. $ID)"
-echo "Id = $ID"
-echo "Weight = $WEIGHT"
-echo "Height = $HEIGHT"
-echo "Order = $ORDER"
-
-if [ ! -f "$CSV" ]; then
-    echo "id,name,weight,height,order" > "$CSV"
+if echo "$RESPONSE" | jq -e .id >/dev/null 2>&1; then
+    ID=$(echo "$RESPONSE" | jq .id)
+    NAME=$(echo "$RESPONSE" | jq -r .name)
+    WEIGHT=$(echo "$RESPONSE" | jq .weight)
+    HEIGHT=$(echo "$RESPONSE" | jq .height)
+    ORDER=$(echo "$RESPONSE" | jq .order)
+    
+    echo "$NAME (No. $ID)"
+    echo "Id = $ID"
+    echo "Weight = $WEIGHT"
+    echo "Height = $HEIGHT"
+    echo "Order = $ORDER"
+    
+    if [ ! -f "$CSV" ]; then
+        echo "id,name,weight,height,order" > "$CSV"
+    fi
+    echo "$ID,$NAME,$WEIGHT,$HEIGHT,$ORDER" >> "$CSV"
+else 
+    echo "Error: No se encontró información para el Pokémon"
+    exit 1
 fi
-echo "$ID,$NAME,$WEIGHT,$HEIGHT,$ORDER" >> "$CSV"
-exit 1
